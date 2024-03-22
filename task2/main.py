03343cfbd5f09f605ca418b5e4ce572ccd678b1d
lMaryanal <maryana.shcherbak@gmail.com>
2024-03-22 22:15:25 +0200

    corrections

--- task2/main.py
+++ task2/main.py
@@ -8,19 +8,43 @@ client = MongoClient(uri, server_api=ServerApi('1'))
 
 db = client.ds_dz02
 # Send a ping to confirm a successful connection
+
+def input_error(func):
+
+    def inner(*args, **kwargs):
+        function_name = str(func).split(" ")[1]
+        try:
+            return func(*args, **kwargs)
+        except ValueError:
+            return "please enter age number"
+        except TypeError:
+            if function_name == 'update_features_by_name':
+                return "Give me name and featur please."
+            if function_name == 'update_age_by_name':
+                return "Give me name and age please."
+            else:
+                return "Give me name please."
+        except Exception as e:
+            return e
+
+    return inner
+
 #виведення всіх записів із колекції
+@input_error
 def read_all():
     result = db.cats.find({})
     return [el for el in result]
 
 #виведення записів із колекції за ім'ям
+@input_error
 def read_name(name):
     result = db.cats.find_one({"name": name})
     if result:
         return result
     else:
         return("information does not exist")
-#оновити вік кота за ім'ям    
+#оновити вік кота за ім'ям
+@input_error    
 def update_age_by_name(name, age):
     if db.cats.find_one({"name": name}):
         db.cats.update_one({"name": name}, {"$set": {"age": int(age)}})
@@ -28,7 +52,11 @@ def update_age_by_name(name, age):
     else:
         return ("name does not exist")
 #додати нову характеристику до списку features кота за ім'ям
+@input_error
 def update_features_by_name(name, featur):
+    if type(featur) is not str:
+        return "please enter one feature string"
+    
     if db.cats.find_one({"name": name}) and type(featur) is str:
         features = db.cats.find_one({"name": name})['features']
         features.append(featur)
@@ -37,6 +65,7 @@ def update_features_by_name(name, featur):
     else:
         return ("name does not exist")
 #видалення запису з колекції за ім'ям тварини
+@input_error
 def delete_by_name(name):
     if db.cats.find_one({"name": name}):
         db.cats.delete_one({"name": name})
@@ -44,6 +73,7 @@ def delete_by_name(name):
     else:
         return ("name does not exist")
 #видалення всіх записів із колекції
+@input_error
 def delete_all():
     db.cats.delete_many({})
     return ("deleted")
@@ -71,13 +101,7 @@ if __name__ == "__main__":
                 },               
             ]
         )
-        #print(read_all())
-        #print(read_name('barsik'))
-        #print(update_age_by_name('barsik', 6))
-        #print(update_features_by_name('barsik', "ласкавий"))
-        #print(delete_by_name('barsik'))
-        #print(delete_all())
-
-        print("success")
+    except ConnectionError:
+        print("connection lost")
     except Exception as e:
         print(e)
